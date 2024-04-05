9e24b03ccd340e9d2a68300e54ff48ee0053800b
seraz1 <alamseraz306@gmail.com>
2024-04-04 17:26:37 -0700

    first commit

---
+++ src/loop/Forloop.java
@@ -0,0 +1,31 @@
+package loop;
+
+public class Forloop {
+
+	public static void main(String[] args) {
+
+		// print table
+		
+   //  int n=4;
+     
+    // for (int i=1; i<=10;i++) {
+    	 
+   // 	 System.out.println(n+"*"+i+"= "+n*i);
+   
+		
+		int n=5;
+		for(int i=1; i<=10; i++) {
+			
+			System.out.println(4*i);
+		}
+     
+  } 	 
+}   	 
+     
+    	 
+
+	
+       
+  
+   
+       
